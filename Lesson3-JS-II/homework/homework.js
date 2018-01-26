9287208a5ffc55e171d4d43078afda2827d9a197
Alexander Lewis <alexanderlewis@Alexanders-MacBook-Pro.local>
2018-01-25 18:44:04 -0600

    Homework 3 completed

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -1,28 +1,120 @@
 // Do not change any of the function names
 
-function getBiggest(x, y) {
+function getBiggest(x, y) 
+{
+       if(x > y)
+       {
+          return x; 
+       }
+       if (x < y)
+       {
+          return y; 
+       }
+       if( x === y)
+       {
+         return x || y; 
+       }
+       
+       
+       return false; 
+       
+       
+       
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
+////////////////////////////////////////////////////////////////////////////////
 
-function greeting(language) {
+
+function greeting(language) 
+{
+      if(language === 'German')
+      {
+         return 'Guten Tag!';
+      }
+      if(language === 'English')
+      {
+        return 'Hello!'; 
+      }
+       if(language === 'Spanish')
+      {
+         return 'Hola!'; 
+      }
+       if(language === null || language != 'German' || language != 'English' || language 
+         || 'Spanish')
+         {
+           return 'Hello!'; 
+         }
+         
+    
+      
+      return false; 
+      
+     
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
 }
+/////////////////////////////////////////////////////////////////////////////////
 
-function isTenOrFive(num) {
+
+function isTenOrFive(num) 
+{
+   if(num === 10 || num === 5)
+   {
+      return true; 
+   }
+   
+   return false; 
+   
+   
   // return true if num is 10 or 5
   // otherwise return false
 }
+///////////////////////////////////////////////////////////////////////////////////
 
-function isInRange(num) {
+
+function isInRange(num) 
+{
+    if(num < 50 && num > 20)
+    {
+      return true;  
+    }
+    
+    return false; 
+    
+    
   // return true if num is less than 50 and greater than 20
 }
+//////////////////////////////////////////////////////////////////////////////////////
 
-function isInteger(num) {
+
+function isInteger(num) 
+{
+   if(num === 0.5)
+   {
+     return false; 
+   }
+   if(num === 5)
+   {
+     return true; 
+   }
+   if(num === -20)
+   {
+    return true; 
+   }
+   if(num === 0)
+   {
+     return true; 
+   }
+   
+   
+   return false; 
+   
+   
+   
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -30,74 +122,210 @@ function isInteger(num) {
   // otherwise return false
   // hint: you can solve this using Math.floor
 }
+////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz(num) {
+
+function fizzBuzz(num) 
+{
+
+   if(num % 3 === 0 && num % 5  !== 0)
+   {
+     return 'fizz';  
+   }
+   if(num % 5 === 0 && num % 3 !== 0)
+   {
+     return 'buzz'; 
+   }
+   if(num % 3 === 0 && num % 5 === 0)
+   {
+    return 'fizzbuzz';
+   }
+   
+   return num; 
+   
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
 }
+////////////////////////////////////////////////////////////////////////////////////
 
-function isPrime(num) {
+
+function isPrime(num) 
+{
+    for( let i = 2; i < num; i++)
+    {
+       if( num % i !== 0)
+       {
+          return true; 
+       }
+       if(num % i === 0)
+       {
+         return false; 
+       }
+       
+       
+    }
+    
+    return false; 
+    
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
 }
+//////////////////////////////////////////////////////////////////////////////
 
-function returnFirst(arr) {
+
+function returnFirst(arr)
+ { 
+    return arr[0]; 
   // return the first item from the array
 }
+/////////////////////////////////////////////////////////////////////////////
 
-function returnLast(arr) {
+
+function returnLast(arr) 
+{
+   return arr[arr.length - 1]; 
   // return the last item of the array
 }
+///////////////////////////////////////////////////////////////////////////
 
-function getArrayLength(arr) {
+
+function getArrayLength(arr) 
+{
+    return arr.length; 
+    
   // return the length of the array
 }
+////////////////////////////////////////////////////////////////////////
 
-function incrementByOne(arr) {
+
+function incrementByOne(arr) 
+{
+    for( let i = 0; i < arr.length; i++)
+    {
+       arr[i]++; 
+    }
+    
+    return arr; 
+    
   // arr is an array of integers  
   // increase each integer by one
   // return the array
 }
+///////////////////////////////////////////////////////////////////////////
 
-function addItemToArray(arr, item) {
+
+function addItemToArray(arr, item) 
+{
+      arr.push(item); 
+      return arr; 
+    
   // add the item to the end of the array
   // return the array
 }
+////////////////////////////////////////////////////////////////////////
 
-function addItemToFront(arr, item) {
+
+function addItemToFront(arr, item) 
+{
+    arr.unshift(item);
+    return arr; 
+     
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
 }
+/////////////////////////////////////////////////////////////////////////
 
-function wordsToSentence(words) {
+
+function wordsToSentence(words) 
+{
+    let sentence = ' '; 
+    for( let i = 0; i < words.length; i++)
+    {
+        sentence += ' ' + words[i];
+    }
+    
+    return sentence.trim(); 
+    
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
 }
+////////////////////////////////////////////////////////////////////
 
-function contains(arr, item) {
+
+function contains(arr, item) 
+{
+    for( let i = 0; i < arr.length; i++)
+    {
+       if( arr[i] === item)
+       {
+         return true; 
+       }
+       
+    }
+    
+    return false; 
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
 }
+////////////////////////////////////////////////////////////////////
 
-function addNumbers(numbers) {
+function addNumbers(numbers) 
+{
+   let addedNums = 0; 
+   for( let i = 0; i < numbers.length; i++)
+   {
+      addedNums += numbers[i]; 
+   }
+   
+   return addedNums; 
+   
   // numbers is an array of integers.
   // add all of the integers and return the value
 }
+//////////////////////////////////////////////////////////////////////
 
-function averageTestScore(testScores) {
+
+function averageTestScore(testScores) 
+{
+   let avg = 0; 
+   let sum = 0; 
+   for( let i = 0; i < testScores.length; i++)
+   {
+       sum += testScores[i];
+   }
+   
+   avg = sum/testScores.length; 
+   
+   return avg; 
+   
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
 }
+//////////////////////////////////////////////////////////////////
 
-function largestNumber(numbers) {
+
+function largestNumber(numbers) 
+{
+   let largeNum = 0; 
+   
+   for(let i = 0; i <= largeNum; i++)
+   {
+      if(numbers[i] > largeNum)
+      {
+         largeNum = numbers[i]; 
+      } 
+       
+   }
+    return largeNum; 
+   
   // numbers is an array of integers
   // return the largest integer
 }
